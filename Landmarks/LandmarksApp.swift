8873a269b6ca43d36bacfa899914dfc3b3f47102
Lorne Day <lornedayy@gmail.com>
2023-09-16 18:01:05 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lorne Day on 2023-09-16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
